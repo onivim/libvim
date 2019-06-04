64e92f2c900583a3597bbbf20f6f9dd7b6ca42db
bryphe <bryphe@outrunlabs.com>
2019-06-04 16:23:52 -0700

    Switch to libvim.h

--- src/libvim.h
+++ src/libvim.h
@@ -1,7 +1,22 @@
+#include "vim.h"
+
 /* libvim.c */
 
+/*
+ * vimInit
+ *
+ * This must be called prior to using any other methods.
+ *
+ * This expects an `argc` and an `argv` parameters,
+ * for the command line arguments for this vim instance.
+ */
 void vimInit(int argc, char **argv);
 
+/*
+ * vimBufferOpen
+ *
+ * Open a buffer and set as current.
+ */
 buf_T *vimBufferOpen(char_u *ffname_arg, linenr_T lnum, int flags);
 
 char_u *vimBufferGetLine(buf_T* buf, linenr_T lnum);
