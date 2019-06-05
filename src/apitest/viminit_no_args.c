e31b8102ad06e1fc29658bdc7b7dface9fe60993
Bryan Phelps <bryphe@outrunlabs.com>
2019-06-05 11:24:36 -0700

    Test case: no args in viminit (#22)

---
+++ src/apitest/viminit_no_args.c
@@ -0,0 +1,8 @@
+#include "libvim.h"
+
+int main(int argc, char **argv) {
+  /* Simple test to validate we can run with no args */
+  char *c[0];
+  vimInit(0, c);
+  printf("Initialized without crashing\n");
+}
