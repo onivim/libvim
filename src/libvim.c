4c81c10c37bfa3578ec324e945f9bb1dc11151ec
Bryan Phelps <bryphe@outrunlabs.com>
2019-06-10 15:59:49 -0700

    Test: Enable remaining curswant tests (#52)
    * Add curswant file
    
    * Update curswant test
    
    * Remove printfs
    
    * Formatting
    
    * Remove update_curswant from execute
    
    * Remove curswant from vimExecute
    
    * Tweak file copying
    
    * Add logging to Makefile
    
    * Logging for mingw build too

--- src/libvim.c
+++ src/libvim.c
@@ -115,7 +115,10 @@ void vimVisualGetRange(pos_T *startPos, pos_T *endPos) {
   }
 }
 
-void vimExecute(char_u *cmd) { do_cmdline_cmd(cmd); }
+void vimExecute(char_u *cmd) { 
+    do_cmdline_cmd(cmd); 
+
+}
 
 int vimGetMode(void) {
     return get_real_state();
