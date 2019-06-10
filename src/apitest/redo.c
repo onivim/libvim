8a5477d7f64d94abfe01b210f7d9723f3fc76e48
Bryan Phelps <bryphe@outrunlabs.com>
2019-06-10 08:11:13 -0700

    Initial redo test (#48)

---
+++ src/apitest/redo.c
@@ -0,0 +1,39 @@
+#include "libvim.h"
+#include "minunit.h"
+
+void test_setup(void) {
+  vimExecute("new");
+}
+
+void test_teardown(void) {}
+
+MU_TEST(test_basic_redo) {
+
+  vimInput("I");
+  vimInput("a");
+  vimInput("b");
+  vimInput("c");
+  vimInput("<esc>");
+
+  mu_check(strcmp(vimBufferGetLine(curbuf, 1), "abc") == 0);
+
+  vimInput(".");
+  mu_check(strcmp(vimBufferGetLine(curbuf, 1), "abcabc") == 0);
+}
+
+MU_TEST_SUITE(test_suite) {
+  MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+  MU_RUN_TEST(test_basic_redo);
+}
+
+int main(int argc, char **argv) {
+  vimInit(argc, argv);
+
+  win_setwidth(5);
+  win_setheight(100);
+
+  MU_RUN_SUITE(test_suite);
+  MU_REPORT();
+  MU_RETURN();
+}
