b33d12f5db3322b1bb966cc6a0ad620fd7ba3484
bryphe <bryphe@outrunlabs.com>
2019-06-11 13:25:21 -0700

    Prototype highlight retrieval

--- src/apitest/search_highlights.c
+++ src/apitest/search_highlights.c
@@ -26,10 +26,71 @@ MU_TEST(test_no_highlights_initially) {
   mu_check(highlights == NULL);
 }
 
+MU_TEST(test_get_highlights) {
+
+  vimInput("/");
+  vimInput("e");
+
+  int num;
+  searchHighlight_T *highlights;
+  vimSearchGetHighlights(0, 0, &num, &highlights);
+
+  int v = 1;
+  int count = 0;
+      pos_T lastPos;
+      pos_T startPos;
+      startPos.lnum = 2;
+      startPos.col = 0;
+      lastPos = startPos;
+
+      pos_T endPos;
+
+  char_u* pattern = vimSearchGetPattern();
+  printf("Pattern: %s\n", pattern);
+
+  while (v == 1) {
+      v = searchit(
+              NULL,
+              curbuf,
+              &startPos,
+              &endPos,
+              FORWARD,
+              pattern,
+              1,
+              SEARCH_KEEP,
+              RE_SEARCH,
+              2,
+              NULL,
+              NULL);
+
+      if (v == 0) {
+          break;
+      }
+
+      if (startPos.lnum < lastPos.lnum || (startPos.lnum == lastPos.lnum && startPos.col <= lastPos.col)) {
+          break;
+      }
+
+      printf("Match number: %d Return value: %d startPos: %d, %d endPos: %d, %d lastPos: %d, %d\n", count, v, startPos.lnum, startPos.col, endPos.lnum, endPos.col, lastPos.lnum, lastPos.col);
+
+
+
+
+      lastPos = startPos;
+
+      startPos = endPos;
+      startPos.col = startPos.col + 1;
+      count++;
+  }
+
+  mu_check(num == 3);
+}
+
 MU_TEST_SUITE(test_suite) {
   MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
   MU_RUN_TEST(test_no_highlights_initially);
+  MU_RUN_TEST(test_get_highlights);
 }
 
 int main(int argc, char **argv) {
