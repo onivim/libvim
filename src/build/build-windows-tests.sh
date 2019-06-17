3505a83538c87f5c008f386a491fb009b2bfa07c
bryphe <bryphe@outrunlabs.com>
2019-06-17 13:49:58 -0700

    Specify AR

--- src/build/build-windows-tests.sh
+++ src/build/build-windows-tests.sh
@@ -1,1 +1,1 @@
-make -f Make_cyg_ming.mak installapitests CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ AR=x86_64-w64-mingw32-ar DESTDIR=$cur__install
+make -f Make_cyg_ming.mak installapitest CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ AR=x86_64-w64-mingw32-ar DESTDIR=$cur__install
