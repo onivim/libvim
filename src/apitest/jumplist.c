4dd299786297abf3ebc9976251b547274f6f6c3b
bryphe <bryphe@outrunlabs.com>
2019-06-21 11:28:49 -0700

    Add edit case

--- src/apitest/jumplist.c
+++ src/apitest/jumplist.c
@@ -26,10 +26,27 @@ MU_TEST(test_jumplist_openfile) {
   mu_check(curbuf == secondBuf);
 }
 
+MU_TEST(test_jumplist_editnew) {
+  buf_T *firstBuf = vimBufferOpen("collateral/testfile.txt", 1, 0);
+
+  vimExecute("e! collateral/a_new_file.txt");
+  buf_T *secondBuf = curbuf;
+
+  mu_check(firstBuf != secondBuf);
+  mu_check(curbuf == secondBuf);
+
+  vimInput("<c-o>");
+  mu_check(curbuf == firstBuf);
+
+  vimInput("<c-i>");
+  mu_check(curbuf == secondBuf);
+}
+
 MU_TEST_SUITE(test_suite) {
   MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
   MU_RUN_TEST(test_jumplist_openfile);
+  MU_RUN_TEST(test_jumplist_editnew);
 }
 
 int main(int argc, char **argv) {
