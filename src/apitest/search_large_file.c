5371ad5121885394c963639c6621d7553fbbc675
bryphe <bryphe@outrunlabs.com>
2019-07-01 06:11:16 -0700

    Add test case

---
+++ src/apitest/search_large_file.c
@@ -0,0 +1,48 @@
+#include "libvim.h"
+#include "minunit.h"
+
+static int cmdLineEnterCount = 0;
+static int cmdLineLeaveCount = 0;
+static int cmdLineChangedCount = 0;
+
+void test_setup(void) {
+  vimInput("<esc>");
+  vimInput("<esc>");
+
+  vimExecute("e!");
+  vimInput("g");
+  vimInput("g");
+  vimInput("0");
+}
+
+void test_teardown(void) {}
+
+MU_TEST(test_search_in_large_file) {
+
+  vimInput("/");
+  printf("Typing e...\n");
+  vimInput("e");
+  printf("Typed e! \n");
+
+  vimSearchGetHighlights(0, 0, &num, &highlights);
+  mu_check(num == 3);
+}
+
+MU_TEST_SUITE(test_suite) {
+  MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+  MU_RUN_TEST(test_search_in_large_file);
+}
+
+int main(int argc, char **argv) {
+  vimInit(argc, argv);
+
+  win_setwidth(5);
+  win_setheight(100);
+
+  buf_T *buf = vimBufferOpen("collateral/large-c-file.c", 1, 0);
+
+  MU_RUN_SUITE(test_suite);
+  MU_REPORT();
+  MU_RETURN();
+}
