747336e6de21b22b261829c3e71ac4e237b0e8e5
bryphe <bryphe@outrunlabs.com>
2019-07-06 09:55:31 -0700

    Remove from a few files

--- src/edit.c
+++ src/edit.c
@@ -1936,9 +1936,6 @@ void ins_redraw(int ready) // not busy with something
    * visible, the command might delete it. */
   if (ready &&
       (has_cursormovedI()
-#ifdef FEAT_TEXT_PROP
-       || popup_visible
-#endif
        ) &&
       !EQUAL_POS(last_cursormoved, curwin->w_cursor))
   {
@@ -1949,10 +1946,6 @@ void ins_redraw(int ready) // not busy with something
       update_curswant();
       ins_apply_autocmds(EVENT_CURSORMOVEDI);
     }
-#ifdef FEAT_TEXT_PROP
-    if (popup_visible)
-      popup_check_cursor_pos();
-#endif
     last_cursormoved = curwin->w_cursor;
   }
 
@@ -4297,17 +4290,6 @@ static void replace_do_bs(int limit_col)
   cc = replace_pop();
   if (cc > 0)
   {
-#ifdef FEAT_TEXT_PROP
-    size_t len_before = 0; // init to shut up GCC
-
-    if (curbuf->b_has_textprop)
-    {
-      // Do not adjust text properties for individual delete and insert
-      // operations, do it afterwards on the resulting text.
-      len_before = STRLEN(ml_get_curline());
-      ++text_prop_frozen;
-    }
-#endif
     if (State & VREPLACE_FLAG)
     {
       /* Get the number of screen cells used by the character we are
@@ -4357,16 +4339,6 @@ static void replace_do_bs(int limit_col)
     // mark the buffer as changed and prepare for displaying
     changed_bytes(curwin->w_cursor.lnum, curwin->w_cursor.col);
 
-#ifdef FEAT_TEXT_PROP
-    if (curbuf->b_has_textprop)
-    {
-      size_t len_now = STRLEN(ml_get_curline());
-
-      --text_prop_frozen;
-      adjust_prop_columns(curwin->w_cursor.lnum, curwin->w_cursor.col,
-                          (int)(len_now - len_before), 0);
-    }
-#endif
   }
   else if (cc == 0)
     (void)del_char_after_col(limit_col);
@@ -5964,9 +5936,6 @@ static int ins_tab(void)
         if ((State & REPLACE_FLAG) && !(State & VREPLACE_FLAG))
           for (temp = i; --temp >= 0;)
             replace_join(repl_off);
-#ifdef FEAT_TEXT_PROP
-        curbuf->b_ml.ml_line_len -= i;
-#endif
       }
       cursor->col -= i;
 
