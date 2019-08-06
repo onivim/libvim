5bf9d3cede51444351ea3bdf91a7efd0bd4bf6e4
bryphe <bryphe@outrunlabs.com>
2019-08-06 07:56:32 -0700

    Add test case for registers

--- src/apitest/registers.c
+++ src/apitest/registers.c
@@ -31,21 +31,21 @@ MU_TEST(test_yank_to_register)
   mu_check(strcmp(lines[0], "This is the first line of a test file") == 0);
 }
 
-MU_TEST(test_yank_to_register)
+MU_TEST(test_delete_to_register)
 {
   vimInput("\"");
-  vimInput("a");
+  vimInput("b");
 
-  vimInput("y");
-  vimInput("y");
+  vimInput("d");
+  vimInput("j");
 
   int numLines;
   char_u **lines;
-  vimRegisterGet('a', &numLines, &lines);
+  vimRegisterGet('b', &numLines, &lines);
 
-  mu_check(numLines == 1);
-  printf("LINE: %s\n", lines[0]);
-  mu_check(strcmp(lines[0], "This is the first line of a test file") == 0);
+  mu_check(numLines == 2);
+  printf("LINE: %s\n", lines[1]);
+  mu_check(strcmp(lines[1], "This is the second line of a test file") == 0);
 }
 
 MU_TEST(test_extra_yank_doesnt_reset)
@@ -74,6 +74,7 @@ MU_TEST_SUITE(test_suite)
   MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
   MU_RUN_TEST(test_yank_to_register);
+  MU_RUN_TEST(test_delete_to_register);
   MU_RUN_TEST(test_extra_yank_doesnt_reset);
 }
 
