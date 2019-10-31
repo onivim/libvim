21c5ae36b14303d8580c812985e9d4d8d4c18d4a
Bryan Phelps <bryphe@outrunlabs.com>
2019-10-31 17:55:08 +0100

    Remove comment

--- src/normal.c
+++ src/normal.c
@@ -3022,16 +3022,6 @@ static void nv_gd(oparg_T *oap, int nchar,
   gotoRequest.location = curwin->w_cursor;
   gotoRequest.target = nchar == 'd' ? DEFINITION : DECLARATION;
   gotoCallback(gotoRequest);
-  /*int len;
-  char_u *ptr;
-
-  if ((len = find_ident_under_cursor(&ptr, FIND_IDENT)) == 0 ||
-      find_decl(ptr, len, nchar == 'd', thisblock, SEARCH_START) == FAIL)
-    clearopbeep(oap);
-#ifdef FEAT_FOLDING
-  else if ((fdo_flags & FDO_SEARCH) && KeyTyped && oap->op_type == OP_NOP)
-    foldOpenCursor();
-#endif*/
 }
 
 /*
