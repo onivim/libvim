3ea028fe16596a58704356dc2626ae31b855e08e
Bryan Phelps <bryphe@outlook.com>
2019-12-14 19:05:52 -0800

    Formatting

--- src/apitest/buffer_set_lines.c
+++ src/apitest/buffer_set_lines.c
@@ -72,7 +72,6 @@ MU_TEST(test_append_after_first_line)
 
 MU_TEST(test_replace_second_line_multiple_lines)
 {
-
   char_u *lines[] = {"new first line", "new second line"};
   vimBufferSetLines(curbuf, 1, 1, lines, 2);
 
@@ -86,6 +85,32 @@ MU_TEST(test_replace_second_line_multiple_lines)
   mu_check(strcmp(vimBufferGetLine(curbuf, 4), "This is the second line of a test file") == 0);
 }
 
+MU_TEST(test_replace_entire_buffer_from_zero)
+{
+  char_u *lines[] = {"abc"};
+  vimBufferSetLines(curbuf, 0, 3, lines, 1);
+  mu_check(vimBufferGetLineCount(curbuf) == 1);
+  mu_check(strcmp(vimBufferGetLine(curbuf, 1), "abc") == 0);
+}
+
+MU_TEST(test_replace_entire_buffer_after_first_line)
+{
+  char_u *lines[] = {"abc"};
+  vimBufferSetLines(curbuf, 1, 3, lines, 1);
+  mu_check(vimBufferGetLineCount(curbuf) == 2);
+  mu_check(strcmp(vimBufferGetLine(curbuf, 1), "This is the first line of a test file") == 0);
+  mu_check(strcmp(vimBufferGetLine(curbuf, 2), "abc") == 0);
+}
+
+MU_TEST(test_replace_entire_buffer_with_more_lines)
+{
+  char_u *lines[] = {"line1", "line2", "line3", "line4", "line5"};
+  vimBufferSetLines(curbuf, 0, 3, lines, 5);
+  mu_check(vimBufferGetLineCount(curbuf) == 5);
+  mu_check(strcmp(vimBufferGetLine(curbuf, 1), "line1") == 0);
+  mu_check(strcmp(vimBufferGetLine(curbuf, 5), "line5") == 0);
+}
+
 MU_TEST_SUITE(test_suite)
 {
   MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
@@ -94,6 +119,9 @@ MU_TEST_SUITE(test_suite)
   MU_RUN_TEST(test_append_after_buffer);
   MU_RUN_TEST(test_append_after_first_line);
   MU_RUN_TEST(test_replace_second_line_multiple_lines);
+  MU_RUN_TEST(test_replace_entire_buffer_from_zero);
+  MU_RUN_TEST(test_replace_entire_buffer_after_first_line);
+  MU_RUN_TEST(test_replace_entire_buffer_with_more_lines);
 }
 
 int main(int argc, char **argv)
--- src/libvim.c
+++ src/libvim.c
@@ -48,17 +48,22 @@ size_t vimBufferGetLineCount(buf_T *buf) { return buf->b_ml.ml_line_count; }
 
 void vimBufferSetLines(buf_T *buf, linenr_T start, linenr_T end, char_u **lines, int count)
 {
-  // Delete from end to start
-  for (int i = end; i > start; i--)
-  {
-    ml_delete_buf(buf, i, FALSE);
-  }
-
   // Append in reverse order... find more efficient strategy though
+  // We append first, because `ml_delete_buf` can't delete the last line,
+  // for replacing entire an buffer contents
   for (int i = count - 1; i >= 0; i--)
   {
-    printf("LINE: %d\n", i);
     ml_append_buf(buf, start, lines[i], 0, FALSE);
+  }
+
+  int deleteCount = end - start;
+  int deleteStart = start + count + 1;
+  int deleteEnd = deleteStart + deleteCount;
+
+  // Delete from end to start
+  for (int i = deleteStart; i < deleteEnd; i++)
+  {
+    ml_delete_buf(buf, deleteStart, FALSE);
   }
 
   changed_lines_buf(buf, start, end, (end - start) - count);
