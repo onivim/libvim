c95efb1944932e637084967537cb0917700cfae8
Bryan Phelps <bryphe@outlook.com>
2020-10-15 10:52:29 -0700

    Fix test that no longer applies

--- src/normal.c
+++ src/normal.c
@@ -7378,10 +7378,13 @@ static void nv_halfpage(cmdarg_T *cap)
 {
   if ((cap->cmdchar == Ctrl_U && curwin->w_cursor.lnum == 1) ||
       (cap->cmdchar == Ctrl_D &&
-       curwin->w_cursor.lnum == curbuf->b_ml.ml_line_count)) {
-        
+       curwin->w_cursor.lnum == curbuf->b_ml.ml_line_count))
+  {
+
     clearopbeep(cap->oap);
-  } else if (!checkclearop(cap->oap)) {
+  }
+  else if (!checkclearop(cap->oap))
+  {
     halfpage(cap->cmdchar == Ctrl_D, cap->count0);
   }
 }
