0ace730b48c7084f84c1bacd661e98fe981f6759
Bryan Phelps <bryphe@outrunlabs.com>
2020-12-09 14:58:49 -0800

    Start wiring up output API

--- src/libvim.c
+++ src/libvim.c
@@ -532,6 +532,10 @@ void vimSetClearCallback(ClearCallback callback)
   clearCallback = callback;
 }
 
+void vimSetOutputCallback(OutputCallback callback) {
+  outputCallback = callback;
+}
+
 void vimSetFormatCallback(FormatCallback callback)
 {
   formatCallback = callback;
